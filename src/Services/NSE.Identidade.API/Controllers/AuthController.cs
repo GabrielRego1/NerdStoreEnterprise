7f95d17f99a1f2fce97ab78fe4904793b304ebed
GabrielRego1 <gabrielantonio280@gmail.com>
2022-12-19 12:06:45 -0300

    add: mensagens em portugues Identity

--- src/Services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/Services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -31,6 +31,7 @@ namespace NSE.Identidade.API.Controllers
         }
         [HttpPost("nova-conta")]
         public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        
         {
             if (!ModelState.IsValid) return CustomResponse(ModelState);
 
