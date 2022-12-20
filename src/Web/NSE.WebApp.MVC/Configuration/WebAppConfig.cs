d8ba923dc1e2c93a99d7030c92865bade2e49cd6
GabrielRego1 <gabrielantonio280@gmail.com>
2022-12-20 19:16:12 -0300

    refatorando AutenticacaoService

--- src/Web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
+++ src/Web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.WebApp.MVC.Extensions;
@@ -8,9 +9,12 @@ namespace NSE.WebApp.MVC.Configuration
 {
     public static class WebAppConfig
     {
-        public static IServiceCollection AddMvcConfiguration(this IServiceCollection services)
+        public static IServiceCollection AddMvcConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddControllersWithViews();
+
+            services.Configure<AppSettings>(configuration);
+
             return services;
         }
 
