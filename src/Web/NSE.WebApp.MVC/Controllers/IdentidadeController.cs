3b9ce6285a98ebd379a641c3cf1da7c4751866a1
GabrielRego1 <gabrielantonio280@gmail.com>
2022-12-19 23:19:37 -0300

    add: login,registro mvc

--- src/Web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/Web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,8 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        [HttpGet("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //TODO: api - registro
+
+            if (false) return View(usuarioRegistro);
+
+            //TODO: Realizar login na APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            //TODO: api - Login
+
+            if (false) return View(usuarioLogin);
+
+            //TODO: Realizar login na APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("sair")]
+        public IActionResult Logout()
+        {
+            //TODO: RETIRAR COOKIES
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
