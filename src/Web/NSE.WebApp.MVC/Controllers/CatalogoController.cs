2df9aa9f321087c869f4803148f0e21d2f26b404
GabrielRego1 <gabrielantonio280@gmail.com>
2022-12-22 01:13:28 -0300

    add: frontend ProdutoList, ProdutoDetalhe

---
+++ src/Web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        [Route("vitrine")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("produto-detalhe/{id}")]
+        public IActionResult ProdutoDetalhe(Guid id)
+        {
+            return View();
+        }
+
+
+    }
+}
