2e56024169db29bbe78dc83926a53aa1c420f3c9
GabrielRego1 <gabrielantonio280@gmail.com>
2022-12-22 00:26:09 -0300

    add: custom Authorize Attribute

---
+++ src/BuildingBlocks/NSE.WebApi.Core/Identidade/Authorize/ClaimsAuthorizeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace NSE.WebApi.Core.Identidade.Authorize
+{
+    internal class ClaimsAuthorizeAttribute : TypeFilterAttribute
+    {
+        public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(RequisitoClaimFilter))
+        {
+            Arguments = new object[] { new Claim(claimName, claimValue) };
+        }
+
+
+    }
+}
---
+++ src/BuildingBlocks/NSE.WebApi.Core/Identidade/Authorize/CustomAuthorization.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace NSE.WebApi.Core.Identidade.Authorize
+{
+    public class CustomAuthorization
+    {
+        public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+        {
+            return context.User.Identity.IsAuthenticated &&
+                   context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+        }
+
+    }
+}
+
