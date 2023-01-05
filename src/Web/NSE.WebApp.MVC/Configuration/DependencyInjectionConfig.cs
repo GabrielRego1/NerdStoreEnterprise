e34b64e9da92bab1ad21230eaa2749c9cfcadced
GabrielRego1 <gabrielantonio280@gmail.com>
2023-01-05 11:27:32 -0300

    add: PollyConfig isolado

--- src/Web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/Web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -18,25 +18,9 @@ namespace NSE.WebApp.MVC.Configuration
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
-
-            var retry = HttpPolicyExtensions
-                  .HandleTransientHttpError()
-                  .WaitAndRetryAsync(new[]
-                  {
-                    TimeSpan.FromSeconds(1),
-                    TimeSpan.FromSeconds(5),
-                    TimeSpan.FromSeconds(10),
-                  }, (outcome, timespan, retryCount, context) =>
-                  {
-                      Console.ForegroundColor = ConsoleColor.Blue;
-                      Console.WriteLine($"Tentando pela {retryCount} vez!");
-                      Console.ForegroundColor = ConsoleColor.White;
-                  });
-
             services.AddHttpClient<ICatalogoService, CatalogoService>()
                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-
-                .AddPolicyHandler(retry);
+               .AddPolicyHandler(PollyConfig.EsperarTentar());
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
