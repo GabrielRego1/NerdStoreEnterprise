425ba06c29b47525c34eaae9e7ea3848a1d29605
GabrielRego1 <gabrielantonio280@gmail.com>
2023-01-11 23:59:52 -0300

    add: RegistrarClienteCommand

---
+++ src/Services/NSE.Cliente.WebAPI/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace NSE.Clientes.WebAPI.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        public void Manipular(RegistrarClienteCommand message)
+        {
+            //validar message
+
+            //persistir no database
+        }
+    }
+}
