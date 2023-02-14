c4af0bba258e653a93ac7380d08fa80c4f5919f5
GabrielRego1 <gabrielantonio280@gmail.com>
2023-02-14 16:19:08 -0300

    add: Setup inicial API Carrinho

---
+++ src/Services/NSE.Carrinho.WebAPI/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Carrinho.WebAPI.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+        }
+    }
+}
