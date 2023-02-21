3d815a2c33ce4f96ae147c742220ede919460354
GabrielRego1 <gabrielantonio280@gmail.com>
2023-02-21 19:36:54 -0300

    add: startup do ApiGateway BFF Compras

---
+++ src/ApiGateways/NSE.BFF.Compras/Configurations/MessageBusConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.BFF.Compras.Configurations
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+        }
+    }
+}
