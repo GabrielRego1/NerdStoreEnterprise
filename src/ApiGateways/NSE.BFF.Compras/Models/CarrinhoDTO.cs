45e95ae4535b25613e5f6b528517cd4046593e41
GabrielRego1 <gabrielantonio280@gmail.com>
2023-03-03 15:25:10 -0300

    fix: formato DTO

--- src/ApiGateways/NSE.BFF.Compras/Models/CarrinhoDTO.cs
+++ src/ApiGateways/NSE.BFF.Compras/Models/CarrinhoDTO.cs
@@ -5,7 +5,9 @@ namespace NSE.BFF.Compras.Models
     public class CarrinhoDTO
     {
         public decimal ValorTotal { get; set; }
-        public decimal Desconto { get; set; }
         public List<ItemCarrinhoDTO> Itens { get; set; } = new List<ItemCarrinhoDTO>();
+        public bool VoucherUtilizado { get; set; }
+        public decimal Desconto { get; set; }
+        public VoucherDTO Voucher { get; set; }
     }
 }
