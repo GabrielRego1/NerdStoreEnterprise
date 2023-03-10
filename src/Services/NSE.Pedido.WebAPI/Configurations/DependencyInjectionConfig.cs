e2c5e5c8c4eaaebbf22739fba1ab347066caacf4
GabrielRego1 <gabrielantonio280@gmail.com>
2023-03-09 23:51:19 -0300

    add: PedidoController add: Commands, Events DI Container

--- src/Services/NSE.Pedido.WebAPI/Configurations/DependencyInjectionConfig.cs
+++ src/Services/NSE.Pedido.WebAPI/Configurations/DependencyInjectionConfig.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using NSE.Core.Mediator;
-using NSE.Pedidos.WebAPI.Application.Queries;
 using NSE.Pedidos.Domain.Pedidos;
 using NSE.Pedidos.Domain.Vouchers;
 using NSE.Pedidos.Infra.Data;
 using NSE.Pedidos.Infra.Data.Repositories;
+using NSE.Pedidos.WebAPI.Application.Commands;
+using NSE.Pedidos.WebAPI.Application.Events;
+using NSE.Pedidos.WebAPI.Application.Queries;
 using NSE.WebApi.Core.Usuario;
 
 namespace NSE.Pedidos.WebAPI.Configurations
@@ -17,6 +21,12 @@ namespace NSE.Pedidos.WebAPI.Configurations
             // API
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
+
+            // Commands
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+
+            // Events
+            services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
 
             // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
