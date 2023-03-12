6944348b8095ed4792fce03dfcb229b7cb1d37d5
GabrielRego1 <gabrielantonio280@gmail.com>
2023-03-11 23:39:03 -0300

    cliente command handlers

--- src/Services/NSE.Cliente.WebAPI/Application/Commands/ClienteCommandHandler.cs
+++ src/Services/NSE.Cliente.WebAPI/Application/Commands/ClienteCommandHandler.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace NSE.Clientes.WebAPI.Application.Commands
 {
-    public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
+    public class ClienteCommandHandler : CommandHandler,
+                                         IRequestHandler<RegistrarClienteCommand, ValidationResult>,
+                                         IRequestHandler<AdicionarEnderecoCommand, ValidationResult>
     {
         private readonly IClienteRepository _clienteRepository;
         public ClienteCommandHandler(IClienteRepository clienteRepository)
@@ -35,6 +37,15 @@ namespace NSE.Clientes.WebAPI.Application.Commands
 
             return await PersistirDados(_clienteRepository.UnitOfWork);
 
+        }
+        public async Task<ValidationResult> Handle(AdicionarEnderecoCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.EhValido()) return message.ValidationResult;
+
+            var endereco = new Endereco(message.Logradouro, message.Numero, message.Complemento, message.Bairro, message.Cep, message.Cidade, message.Estado, message.ClienteId);
+            _clienteRepository.AdicionarEndereco(endereco);
+
+            return await PersistirDados(_clienteRepository.UnitOfWork);
         }
     }
 }
--- src/Services/NSE.Cliente.WebAPI/Configurations/DependencyInjectionConfig.cs
+++ src/Services/NSE.Cliente.WebAPI/Configurations/DependencyInjectionConfig.cs
@@ -6,7 +6,6 @@ using NSE.Clientes.WebAPI.Application.Events;
 using NSE.Clientes.WebAPI.Data.Contexts;
 using NSE.Clientes.WebAPI.Data.Repositories;
 using NSE.Clientes.WebAPI.Models;
-using NSE.Clientes.WebAPI.Services;
 using NSE.Core.Mediator;
 
 namespace NSE.Clientes.WebAPI.Configuration
@@ -23,7 +22,7 @@ namespace NSE.Clientes.WebAPI.Configuration
 
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IRequestHandler<RegistrarClienteCommand, ValidationResult>, ClienteCommandHandler>();
-
+            services.AddScoped<IRequestHandler<AdicionarEnderecoCommand, ValidationResult>, ClienteCommandHandler>();
         }
     }
 }
